7ce08f14ab37544695477e866b37c38c1cf2392f
Vladimir Shchur <vs3@sanger.ac.uk>
2015-02-05 09:23:18 +0000

    Sampling cluster distance added. Improved accuracy.

--- analyzeARG.py
+++ analyzeARG.py
@@ -2,13 +2,13 @@
 import sys
 import re
 import os
+from math import sqrt
 
 def num(s):
     try:
         return int(s)
     except ValueError:
         return float(s)
-
 
 
 def MinBranchesAverage(string, density, n):
@@ -21,10 +21,6 @@ def MinBranchesAverage(string, density, n):
         su = '\('+m.group(1)+','+m.group(2)+'\)'
         s = re.compile(su)
         string = s.sub(str(int(m.group(1))+int(m.group(2))), string, 1)
-
-
-
-
 
 
 def MinBranches(string, density, n):
@@ -41,8 +37,6 @@ def MinBranches(string, density, n):
                 density[int(h2)][int(h1)][ms] += 1
         r = "["+m.group(1)+','+m.group(2)+']'
         string = re.sub(s, r, string, 1)
-
-
 
 
 def WriteListInFile(arr, fname, header = ""):
@@ -110,14 +104,29 @@ def Write3DListInFile(arr, dir, fname, mode):
         f.close()
 
 
+def WriteClusterDensity(arr, fname, header = ""):
+    if header == "":
+        header = fname
+    with open(fname, 'w') as f:
+        f.write(header+"\n")
+        for el in arr:
+            for i in range(len(el)):
+                f.write(str(el[i]))
+                if i < len(el) - 1:
+                    f.write(',')
+                else:
+                    f.write('\n')
 
 
-
-def InitializeLists(mode, arr, n):
-    if mode == "-a":
+def InitializeLists(dim, arr, n):
+    if dim == 1:
         for i in range(n):
             arr.append(0)
-    if mode == "-i":
+    if dim == 2:
+        for i in range(n):
+            ar1 = [0]*n
+            arr.append(ar1)
+    if dim == 3:
         for i in range(n):
             den1 = []
             for j in range(n):
@@ -126,6 +135,35 @@ def InitializeLists(mode, arr, n):
             arr.append(den1)
 
 
+def InitializePairs(pairs, brDyn, M, scd):
+    for i in range(M):
+        for j in range(scd):
+            k = sqrt(47*((j+103)%17))
+            k = ((73+i+k)**2)%M
+            k = int(k)
+            pairs.append([i, k])
+            emptyVector = []
+            brDyn.append(emptyVector)
+
+
+def ClusterDistances(string, pairs, brDyn, lDensity, n):
+    string = re.sub(r"(\d+)", r"[\1]", string)
+    for i in range(n-1):
+        s = re.compile("\(\[([0-9,]+)\],\[([0-9,]+)\]\)")
+        m = re.search(s, string)
+        g1 = m.group(1).split(",")
+        g2 = m.group(2).split(",")
+        ms = len(g1) + len(g2) -1
+        for h1 in g1:
+            for h2 in g2:
+                lDensity[int(h1)][int(h2)] = ms
+                lDensity[int(h2)][int(h1)] = ms
+        r = "["+m.group(1)+','+m.group(2)+']'
+        string = re.sub(s, r, string, 1)
+    for i in range(len(pairs)):
+        h1 = pairs[i][0]
+        h2 = pairs[i][1]
+        brDyn[i].append(lDensity[h2][h1])
 
 print "Clustering method: read and analyze trees in Newick format"
 #n=10
@@ -135,17 +173,18 @@ except IndexError:
     print "Set mode: -a for the average over the population, -i for one-to-one, -h for help"
     sys.exit()
 
-if mode != "-a" and mode != "-i" and mode != "-h":
-    print "Set mode: -a for the average over the population, -i for one-to-one, -h for help"
+if mode != "-a" and mode != "-i" and mode != "-c" and mode != "-h":
+    print "Set mode: -a for the average over the population, -i for one-to-one, -c for sampling cluster distances, -h for help"
     sys.exit()
 
 if mode == "-h":
-    print "First parameter is the mode. Use -a for the average over the population, -i for one-to-one, -h for help."
+    print "First parameter is the mode. Use -a for the average over the population, -i for one-to-one, -c for sampling cluster distances, -h for help."
     print "Second parameter specifies the filename."
-    print "-ll [integer]: set the line limit (the number of trees to be processed)."
-    print "-sl [integer]: set the sampling parameter."
-    print "-of [tab, csv]: set the output format (\'csv\' by default)."
-    print "-lc [integer]: set the line counter."
+    print "-ll  [integer]: set the line limit (the number of trees to be processed)."
+    print "-sl  [integer]: set the sampling parameter."
+    print "-of  [tab, csv]: set the output format (\'csv\' by default)."
+    print "-lc  [integer]: set the line counter."
+    print "-scd [integer]: adjust the number of samples of cluster distances."
     sys.exit()
 
 try:
@@ -158,6 +197,7 @@ ll = -1
 sl = 1
 of = "csv"
 lc = 500
+scd = 5
 for i in range(3, len(sys.argv)):
     if sys.argv[i] == "-ll":
         i += 1
@@ -176,7 +216,13 @@ for i in range(3, len(sys.argv)):
         try:
             lc = num(sys.argv[i])
         except IndexError:
-            print "Cannot set sampling parameter, the the default value \'500\' will be used."
+            print "Cannot set sampling parameter, the default value \'500\' will be used."
+    elif sys.argv[i] == "-scd":
+        i += 1
+        try:
+            scd = num(sys.argv[i])
+        except IndexError:
+            print "Cannot set sampling adjuster for cluster distances, the default value \'5\' will be used."
     elif sys.argv[i] == "-of":
         i += 1
         try:
@@ -191,34 +237,46 @@ f = open(fname, 'r')
 n = -1
 counter = 0
 density = []
+lDensity = []
 densityAverage = []
+pairs = []
+brDyn = []
 
 for line in f:
     if line.startswith("NEWICK_TREE:"):
+        tree = line
+    elif line.startswith("SITE:"):
         counter += 1
         if (counter-1)%sl != 0:
             continue
         s = re.compile('NEWICK_TREE:\t\[[0-9]+\]|\:[0-9]+\.[0-9e\-]+')
-        line = s.sub('', line)
+        tree = s.sub('', tree)
         if n == -1:
-            n = line.count("(") + 1
+            n = tree.count("(") + 1
             print str(n) + " leaves."
             if mode == "-a":
-                InitializeLists(mode, densityAverage, n)
+                InitializeLists(1, densityAverage, n)
             if mode == "-i":
-                InitializeLists(mode, density, n)
+                InitializeLists(3, density, n)
+            if mode == "-c":
+                InitializeLists(2, lDensity, n)
+                InitializePairs(pairs, brDyn, n, scd)
+              #  for el in pairs:
+                    #print `el[0]` + " " + `el[1]`
         else:
-            if n-1 != line.count("("):
+            if n-1 != tree.count("("):
                 print "Wrong number of leaves. Aborted."
                 sys.exit()
-#        line = '(((0,1),(2,3)),(4,5))'
-#        print line[0:100]
+#        tree = '(((0,1),(2,3)),(4,5))'
+#        print tree[0:100]
         if mode == "-a":
-            MinBranchesAverage(line, densityAverage, n)
+            MinBranchesAverage(tree, densityAverage, n)
         if mode == "-i":
-            MinBranches(line, density, n)
+            MinBranches(tree, density, n)
+        if mode == "-c":
+            ClusterDistances(tree, pairs, brDyn, lDensity, n)
         if counter%lc == 0:
-            print str(counter) + ' lines analyzed'
+            print str(counter) + ' sites analyzed'
         if counter == ll*sl:
             break
 f.close()
@@ -226,3 +284,6 @@ if mode == "-a":
     WriteListInFile(densityAverage, "densityAverage.txt", header = "MACS_correct")
 if mode == "-i":
     Write3DListInFile(density, "data", "densityOneToOne", of)
+if mode == "-c":
+    print len(pairs)
+    WriteClusterDensity(brDyn, "clusterDist_macs.txt", header = "")
